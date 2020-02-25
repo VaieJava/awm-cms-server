767d29e80b40605d07e9f0e1e6cb27cba02f6e54
vaie <vaielx@163.com>
2020-02-26 00:09:36 +0800

    init

---
+++ src/main/java/com/outdd/cms/AwmCmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.outdd.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwmCmsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwmCmsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/outdd/cms/AwmCmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outdd.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwmCmsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
